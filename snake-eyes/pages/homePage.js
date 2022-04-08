ed84be7971c4440840016aa1c756cf90f3981c22
klat61012 <43784651+klat61012@users.noreply.github.com>
2022-04-08 04:53:45 -0500

    previousCommitWasn'tDoneProperly

---
+++ snake-eyes/pages/homePage.js
@@ -0,0 +1,27 @@
+import styles from "../styles/homePage.module.css";
+
+
+export default function homePage(){
+    return(
+    <div className={styles.container}>
+        <div>
+            Grid1 
+        </div>
+        <div>
+           grid2
+        </div>
+        <div>
+            grid3
+        </div>
+        <div>
+            Grid1 
+        </div>
+        <div>
+           grid2
+        </div>
+        <div>
+            grid3
+        </div>
+    </div>
+    )
+}
