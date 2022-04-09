e66853334dd1c7e364580fa42a6351233f813580
klat61012 <43784651+klat61012@users.noreply.github.com>
2022-04-09 07:12:04 -0500

    Fixed the layout of enterpage

---
+++ snake-eyes/pages/test.js
@@ -0,0 +1,19 @@
+import styles from "../styles/test.module.css";
+
+export default function test(){
+
+    return(
+        <div className={styles.container}>
+                <img className={styles.clubImage}
+                src="/homePage.png"    
+                />  
+           
+        </div>
+
+
+
+
+
+
+    )
+}
