c4559799007e29df4c0318320cec20b25ad08aab
klat61012 <43784651+klat61012@users.noreply.github.com>
2022-04-12 07:41:40 -0500

    Added modal for mobile nav. Worked on home page layout.

---
+++ snake-eyes/components/Modal.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import styles from "../styles/modal.module.css";
+
+export default function Modal({ show, onClose, children}) {
+        const [isBrowser, setIsBrowser] = useState(false);
+      
+        useEffect(() => {
+          setIsBrowser(true);
+        }, []);
+      
+        const handleCloseClick = (e) => {
+          e.preventDefault();
+          onClose();
+        };
+      
+        const modalContent = show ? (
+            <div className={styles.overlay}>
+                <div className={styles.modal}>
+                    <div className={styles.header}>
+                        <img className={styles.SEheader}
+                        src="/finalLogoWithSE.png"
+                        >
+                        </img>
+                        <a className={styles.closeModal} href = "#" onClick={handleCloseClick}>x
+                        </a>
+                    </div>
+                    <div className={styles.body}>{children}</div>
+                </div>
+            </div>
+         
+        ): null;
+        if(isBrowser){
+            return ReactDOM.createPortal(
+                modalContent, document.getElementById("modal-root")
+        );
+    } else {
+        return null;
+    }
+}
+
+
---
+++ snake-eyes/pages/navPage.js
@@ -0,0 +1,32 @@
+import styles from "../styles/navPage.module.css";
+
+export default function navPage(){
+    return(
+        <div className={styles.container}>
+        <div>
+            <h1>Buy Now</h1>
+          
+        </div>
+        <div>
+            <h1>About us</h1>
+            
+        </div>
+        <div>
+            <h1>FAQ</h1>
+        </div>
+        <div>
+            <h1>Test</h1>
+        </div>
+        </div>
+
+
+
+
+
+
+    )
+
+
+
+
+}
